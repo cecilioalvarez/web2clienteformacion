277cbbc70252761da1555938a54d335296de9ca3
cursos <cursos@host.docker.internal>
2024-06-04 12:55:10 +0200

    post peticion

--- web1cliente/src/main/java/com/arquitecturajava/web1cliente/controllers/AlumnoController.java
+++ web1cliente/src/main/java/com/arquitecturajava/web1cliente/controllers/AlumnoController.java
@@ -4,9 +4,12 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
 import com.arquitecturajava.web1cliente.models.Alumno;
@@ -38,5 +41,24 @@ public class AlumnoController {
 
 	}
 
+	
+	
+	@PostMapping("/insertaralumno")
+	public String formularioAlumno(@ModelAttribute Alumno alumno, Model modelo) {
+
+		//alumnoExamenesService.insertar(alumno);
+
+		//List<Alumno> lista = alumnoExamenesService.buscarTodosAlumnos();
+
+		// asigna a la plantilla una nombre de lista junto con los datos
+		
+		
+		ResponseEntity<Alumno> respuesta=plantilla.postForEntity(servidorExamenes+"/webapi/alumnos", alumno, Alumno.class);
+		System.out.println(respuesta.getStatusCode());
+		Alumno[] listaArray=plantilla.getForEntity(servidorExamenes+"/webapi/alumnos", Alumno[].class).getBody();
+		modelo.addAttribute("lista",Arrays.asList(listaArray) );
+		return "listaalumnos.xhtml";
+
+	}
 	
 }
