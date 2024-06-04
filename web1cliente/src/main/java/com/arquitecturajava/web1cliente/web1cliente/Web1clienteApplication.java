f2a8b8f346efc0afbc0a1d850cfe9c41ced7a7d9
cursos <cursos@host.docker.internal>
2024-06-04 11:34:15 +0200

    inicio

---
+++ web1cliente/src/main/java/com/arquitecturajava/web1cliente/web1cliente/Web1clienteApplication.java
@@ -0,0 +1,13 @@
+package com.arquitecturajava.web1cliente.web1cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web1clienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web1clienteApplication.class, args);
+	}
+
+}
---
+++ web1cliente/src/test/java/com/arquitecturajava/web1cliente/web1cliente/Web1clienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitecturajava.web1cliente.web1cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web1clienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
