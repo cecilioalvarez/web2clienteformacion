3716e6ce32d684db9cb328f355ea9f3d480d13bb
cursos <cursos@host.docker.internal>
2024-06-04 12:21:10 +0200

    nuevo modificacion

--- web1cliente/src/main/java/com/arquitecturajava/web1cliente/controllers/AlumnoController.java
+++ web1cliente/src/main/java/com/arquitecturajava/web1cliente/controllers/AlumnoController.java
@@ -3,6 +3,7 @@ package com.arquitecturajava.web1cliente.controllers;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +16,15 @@ public class AlumnoController {
 
 	@Autowired
 	RestTemplate plantilla;
+	@Value("${servidor.examenes}")
+	private String servidorExamenes;
 	
 	@GetMapping("/listaalumnos")
 	public String listaAlumnos(Model modelo) {
 		
 	
 		
-		Alumno[] listaArray=plantilla.getForEntity("http://localhost:8080/webapi/alumnos", Alumno[].class).getBody();
+		Alumno[] listaArray=plantilla.getForEntity(servidorExamenes+"/webapi/alumnos", Alumno[].class).getBody();
 
 	
 		modelo.addAttribute("lista",Arrays.asList(listaArray) );
